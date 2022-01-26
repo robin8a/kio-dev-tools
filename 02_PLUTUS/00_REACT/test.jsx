f1ede2b87e79416e717f868c59275b2909ac11ea
ROBIN OCHOA <robin8a@local.mac>
2022-01-26 09:38:38 -0500

    updated jsx snippets

---
+++ 02_PLUTUS/00_REACT/test.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+// Bootstrap
+import { Container, Button } from 'react-bootstrap'
+
+export default class test extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
+    }
+
+    render() {
+        return (
+            <div>
+                
+                
+            </div>
+        )
+    }
+}
